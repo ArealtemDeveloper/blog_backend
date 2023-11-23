60d5df8d5d2736ae761eaf181990c57a2ec14394
Alexandr <seriousguytuwan@gmail.com>
2023-11-23 10:46:30 +0300

    Backend init

---
+++ db.js
@@ -0,0 +1,8 @@
+import mysql from "mysql"
+
+export const db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password: "Arealtem0503#",
+  database:"blog"
+})
---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(process.env.PORT || 8800, () => {
+  console.log("Connected!");
+});
