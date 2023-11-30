71bc997846127cb057e29dea01c06c3bb4ec1bb5
Alexandr <seriousguytuwan@gmail.com>
2023-11-30 16:57:09 +0300

    Added pagination route

--- routes/posts.js
+++ routes/posts.js
@@ -6,12 +6,14 @@ import {
   getPosts,
   updatePost,
   getPostsByQuery,
+  getPostsByPage,
 } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
+router.get("/page/get", getPostsByPage)
 router.post("/search/get", getPostsByQuery)
 router.post("/", addPost);
 router.delete("/:id", deletePost);
